9dd527026476a59aac94f60e2d1fbb235ae10df5
karlwoien <karlwoien@gmail.com>
2024-11-18 14:34:19 +0100

    adds and update custom colors

--- src/components/header/Nav/index.jsx
+++ src/components/header/Nav/index.jsx
@@ -3,12 +3,12 @@ export default function Nav () {
         <nav>
             <ul className="flex items-center space-x-4">
                 <li>
-                    <a href="/" className="text-white hover:text-red-500 transition-colors">
+                    <a href="/" className="text-white hover:text-megablue transition-colors">
                         Home
                     </a>
                 </li>
                 <li>
-                    <a href="/contact" className="text-white hover:text-red-500 transition-colors">
+                    <a href="/contact" className="text-white hover:text-megablue transition-colors">
                         Contact
                     </a>
                 </li>
