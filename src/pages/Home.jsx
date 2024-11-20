5d8325d44bc22993b9950555cb05a130e7c2302e
karlwoien <karlwoien@gmail.com>
2024-11-20 18:54:32 +0100

    adds initial content and components to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,43 @@
-export default function Home () {
+import { useProducts } from "../Hooks/UseProducts";
+import SearchBar from "../components/SearchBar";
+import ProductCard from "../components/ProductCard";
+
+export default function Home() {
+    const {products, loading} = useProducts();
+
     return (
-        <h1>Home page</h1>
+        <div>
+            {/*Hero section*/}
+            <section className="py-20 md:py-32 flex items-center justify-center text-center px-6 md:px-12">
+                <div className="max-w-4xl">
+                    <h1 className="text-5xl md:text-6xl text-megablue uppercase font-semibold">
+                        Discover the Best Deals on Products You Love
+                    </h1>
+                    <p className="text-xl mt-8">
+                        Shop smarter with MegaBuy. From tech gadgets to fashion finds, we’ve got something for everyone. Unbeatable prices, exclusive offers, and fast delivery — only a click away!
+                    </p>
+                </div>
+            </section>
+
+            {/*SearchBar*/}
+            <section className="pt-8">
+                <div className="max-w-6xl mx-auto">
+                    <SearchBar />
+                </div>
+            </section>
+
+            {/*Product section*/}
+            <section className="py-8 max-w-6xl mx-auto">
+                <div className="px-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+                    {loading ? (
+                        <p>Loading products...</p>
+                    ) : (
+                        products.map((product) => (
+                            <ProductCard key={product.id} product={product} />
+                        ))
+                    )}
+                </div>
+            </section>
+        </div>
     );
 };
