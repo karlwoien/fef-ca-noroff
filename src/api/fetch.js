8d2be2c4f3d1ea5eb21ec7a7d39cd659b255a14a
karlwoien <karlwoien@gmail.com>
2024-11-26 15:15:18 +0100

    implement getProductByID and integrate product details in ProductDetails page

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -15,6 +15,19 @@ export async function getProducts () {
     };
 };
 
-export async function getProductsByID (id) {
+export async function getProductByID(id) {
+    try {
+        const response = await fetch(`${BASE_URL}/${id}`);
 
+        if (!response.ok) {
+            throw new Error("Failed to fetch product");
+        }
+
+        const { data } = await response.json();
+        console.log("Fetched product data:", data);
+        return data;
+    } catch (error) {
+        console.error("Error fetching product by ID:", error.message);
+        throw new Error(error.message);
+    }
 };
