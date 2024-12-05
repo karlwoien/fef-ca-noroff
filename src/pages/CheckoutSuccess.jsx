72945ee16a2335f68d9f909001c50be965a0a296
karlwoien <karlwoien@gmail.com>
2024-12-05 13:52:03 +0100

    adds more info/content and styling to checkout-success

--- src/pages/CheckoutSuccess.jsx
+++ src/pages/CheckoutSuccess.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from 'react';
 import { useCartStore } from '../components/Store';
+import { Link } from 'react-router-dom';
+import { AiOutlineCheckCircle } from 'react-icons/ai';
 
 export default function CheckoutSuccess() {
     const clearCart = useCartStore((state) => state.clearCart);
@@ -9,9 +11,29 @@ export default function CheckoutSuccess() {
     }, [clearCart]);
 
     return (
-        <div className="">
-            <h1 className="text-3xl font-bold">Checkout Successful</h1>
-            <p>Thank you for your purchase!</p>
+        <div className="flex flex-col items-center justify-center">
+            {/* Success Icon */}
+            <AiOutlineCheckCircle className="text-megablue text-9xl mb-4" />
+
+            {/* Success Message */}
+            <h1 className="text-5xl font-bold mb-4 ">Checkout Successful</h1>
+            <p className="text-lg text-center mb-8">
+                
+                We hope you'll be happy with your purchase. <br />
+                Please don't hesitate to{' '}
+                <Link to="/contact" className="text-megablue underline">
+                    contact
+                </Link>{' '}
+                us if you have any questions.
+            </p>
+
+            {/* Shop More Button */}
+            <Link
+                to="/"
+                className="bg-megablue text-white py-3 px-6 rounded hover:bg-megablue-dark transition"
+            >
+                Shop More!
+            </Link>
         </div>
     );
 }
