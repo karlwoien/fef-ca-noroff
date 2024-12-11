fa3f4b9a31a3b3d4b7de1dbc4687fc6a4dd5a190
karlwoien <karlwoien@gmail.com>
2024-12-11 13:49:28 +0100

    adds calculate discount function and discount to product cards

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,7 +1,10 @@
-import { Link } from "react-router-dom";
 import LinkButton from "../LinkButton";
+import { calculateDiscount } from "../../utils/discount";
 
 export default function ProductCard({ product }) {
+    const discount = calculateDiscount(product.price, product.discountedPrice);
+    const hasDiscount = discount !== null;
+
     return (
         <div className="card flex flex-col justify-between rounded-lg border h-full">
             {/* Image */}
@@ -16,7 +19,14 @@ export default function ProductCard({ product }) {
             {/* Content */}
             <div className="m-4 flex-grow flex flex-col">
                 <h3 className="mt-2 text-lg font-semibold">{product.title}</h3>
-                <p className="mt-2">NOK {product.price}</p>
+                <div className="mt-2 flex items-center justify-between">
+                    <p className="text-base">NOK {product.price}</p>
+                    {hasDiscount && (
+                        <span className="text-green-600 border border-green-600 px-2 py-1 rounded text-sm">
+                            -{discount}%
+                        </span>
+                    )}
+                </div>
             </div>
 
             {/* Link */}
